8a3b0bdc02d37ecca66305207c327e5b180deb7b
Um... Figure it out! <33612115+CTH999@users.noreply.github.com>
2025-08-17 10:28:31 -0500

    compress

--- src/main/java/zmaster587/advancedRocketry/api/ISpaceObjectManager.java
+++
@@ -1,8 +0,0 @@
-package zmaster587.advancedRocketry.api;
-
-import net.minecraft.util.math.BlockPos;
-import zmaster587.advancedRocketry.api.stations.ISpaceObject;
-
-public interface ISpaceObjectManager {
-	public ISpaceObject getSpaceStationFromBlockCoords(BlockPos pos);
-}
