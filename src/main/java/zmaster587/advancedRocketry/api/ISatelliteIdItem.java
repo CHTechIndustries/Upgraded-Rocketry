99855ef5632c39e1e6375a1a438fdd12b083743b
Um... Figure it out! <33612115+CTH999@users.noreply.github.com>
2025-08-17 11:31:36 -0500

    compress

--- src/main/java/zmaster587/advancedRocketry/api/ISatelliteIdItem.java
+++
@@ -1,8 +0,0 @@
-package zmaster587.advancedRocketry.api;
-
-import net.minecraft.item.ItemStack;
-import zmaster587.advancedRocketry.api.satellite.SatelliteProperties;
-
-public interface ISatelliteIdItem {
-	public void setSatellite(ItemStack stack, SatelliteProperties properties);
-}
