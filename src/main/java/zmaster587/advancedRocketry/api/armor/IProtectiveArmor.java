3e4ae62f8af0ceff264a3b1c7d56319b8dcb12e5
Um... Figure it out! <33612115+CTH999@users.noreply.github.com>
2025-08-17 10:26:24 -0500

    compress stable branch

--- src/main/java/zmaster587/advancedRocketry/api/armor/IProtectiveArmor.java
+++
@@ -1,7 +0,0 @@
-package zmaster587.advancedRocketry.api.armor;
-
-import zmaster587.advancedRocketry.api.IAtmosphere;
-
-public interface IProtectiveArmor {
-	public boolean protectsFromSubstance(IAtmosphere atmosphere);
-}
